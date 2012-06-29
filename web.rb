ee380fbd5b2765daa142618baf1639fab8d004da
Colin Rymer <crymer11@gmail.com>
2012-06-29 18:36:32 -0400

    refactoring like someone who really has to go to the bathroom

--- web.rb
+++ web.rb
@@ -6,21 +6,26 @@ Wordnik.configure do |config|
     config.api_key = '2511533437086d533fd5103847108c1b84ca3abec199e412d'
 end
 
+def get_comment(videos)
+  client.comments(videos.sample.video_id.split(":").last).sample
+end
+
+def random_word
+  Wordnik.words.get_random_word(:hasDictionaryDef => 'true')["word"]
+end
+
+def search_for_videos(word)
+  client.videos_by(query: word).videos
+end
+
+def client
+  @client ||= YouTubeIt::Client.new
+end
+
 # Routes
 get '/' do
-  word = Wordnik.words.get_random_word(:hasDictionaryDef => 'true')
-  word["word"]
-  client = YouTubeIt::Client.new
-  videos = client.videos_by(query: word["word"]).videos
-  while videos.length < 1
-    videos = client.videos_by(
-      query: Wordnik.words.get_random_word(:hasDictionaryDef => 'true')["word"]
-    ).videos
-  end
-  comment = client.comments(videos.sample.video_id.split(":").last).sample
-  while comment.nil?
-    comment = client.comments(videos.sample.video_id.split(":").last).sample
-  end
-  @comment = comment.content
+  @videos = search_for_videos(random_word) while @videos.nil? || @videos.length < 1
+  @comment_response = get_comment(@videos) while @comment_response.nil? || @comment_response.content.nil?
+  @comment = @comment_response.content
   erb :index
 end
